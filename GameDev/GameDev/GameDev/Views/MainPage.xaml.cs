bc0c784432fe5b6140dab01482610965eaa6d69d
Karthikeyan Reddy Rokkam <rokkamkarthi@seattleu.edu>
2019-01-15 22:16:33 -0800

    removed Browse Option

--- GameDev/GameDev/GameDev/Views/MainPage.xaml.cs
+++ GameDev/GameDev/GameDev/Views/MainPage.xaml.cs
@@ -26,9 +26,9 @@ namespace GameDev.Views
             {
                 switch (id)
                 {
-                    case (int)MenuItemType.Browse:
+                    /*case (int)MenuItemType.Browse:
                         MenuPages.Add(id, new NavigationPage(new ItemsPage()));
-                        break;
+                        break;*/
                     case (int)MenuItemType.About:
                         MenuPages.Add(id, new NavigationPage(new AboutPage()));
                         break;
--- GameDev/GameDev/GameDev/Views/MenuPage.xaml.cs
+++ GameDev/GameDev/GameDev/Views/MenuPage.xaml.cs
@@ -18,7 +18,7 @@ namespace GameDev.Views
 
             menuItems = new List<HomeMenuItem>
             {
-                new HomeMenuItem {Id = MenuItemType.Browse, Title="Browse" },
+                //new HomeMenuItem {Id = MenuItemType.Browse, Title="Browse" },
                 new HomeMenuItem {Id = MenuItemType.About, Title="About" },
                 new HomeMenuItem {Id = MenuItemType.Item, Title="Item" }
             };
