a0af9d74a5cbc25c9f8a3f4fe43e40cd1e9ceb3c
Karthikeyan Reddy Rokkam <rokkamkarthi@seattleu.edu>
2019-01-18 00:16:32 -0800

    Modified Main page to Tabbed look

--- GameDev/GameDev/GameDev/Views/MainPage.xaml.cs
+++ GameDev/GameDev/GameDev/Views/MainPage.xaml.cs
@@ -1,51 +1,20 @@
-﻿using GameDev.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace GameDev.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class MainPage : MasterDetailPage
+    public partial class MainPage : TabbedPage
     {
-        Dictionary<int, NavigationPage> MenuPages = new Dictionary<int, NavigationPage>();
-        public MainPage()
+        public MainPage ()
         {
             InitializeComponent();
-
-            MasterBehavior = MasterBehavior.Popover;
-
-            MenuPages.Add((int)MenuItemType.About, (NavigationPage)Detail);
-        }
-
-        public async Task NavigateFromMenu(int id)
-        {
-            if (!MenuPages.ContainsKey(id))
-            {
-                switch (id)
-                {
-                    case (int)MenuItemType.About:
-                        MenuPages.Add(id, new NavigationPage(new AboutPage()));
-                        break;
-                    case (int)MenuItemType.Manage:
-                        MenuPages.Add(id, new NavigationPage(new ManagePage()));
-                        break;
-                }
-            }
-
-            var newPage = MenuPages[id];
-
-            if (newPage != null && Detail != newPage)
-            {
-                Detail = newPage;
-
-                if (Device.RuntimePlatform == Device.Android)
-                    await Task.Delay(100);
-
-                IsPresented = false;
-            }
         }
     }
 }
