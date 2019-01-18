e35e755a680fc179875f2226504b3143fa8e1742
Karthikeyan Reddy Rokkam <rokkamkarthi@seattleu.edu>
2019-01-17 23:28:43 -0800

    Added CRUDi Pages to Items, Characers, Monsters & Score

---
+++ GameDev/GameDev/GameDev/Views/Items/ItemEditPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GameDev.Views.Items
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ItemEditPage : ContentPage
+	{
+		public ItemEditPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
