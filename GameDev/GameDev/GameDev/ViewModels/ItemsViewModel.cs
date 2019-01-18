708b28ca30cf028fed3d64f18279748280cdfc80
Karthikeyan Reddy Rokkam <rokkamkarthi@seattleu.edu>
2019-01-17 23:48:41 -0800

    updated new file name

--- GameDev/GameDev/GameDev/ViewModels/ItemsViewModel.cs
+++ GameDev/GameDev/GameDev/ViewModels/ItemsViewModel.cs
@@ -21,7 +21,7 @@ namespace GameDev.ViewModels
             Items = new ObservableCollection<Item>();
             LoadItemsCommand = new Command(async () => await ExecuteLoadItemsCommand());
 
-            MessagingCenter.Subscribe<NewItemPage, Item>(this, "AddItem", async (obj, item) =>
+            MessagingCenter.Subscribe<ItemNewPage, Item>(this, "AddItem", async (obj, item) =>
             {
                 var newItem = item as Item;
                 Items.Add(newItem);
