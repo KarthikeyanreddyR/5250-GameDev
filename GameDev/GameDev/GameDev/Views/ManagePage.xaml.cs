16796cdee8618860c65a9bac35501d6a45405a0c
Karthikeyan Reddy Rokkam <rokkamkarthi@seattleu.edu>
2019-01-20 17:59:27 -0800

    Modified manage page to tabbed view

--- GameDev/GameDev/GameDev/Views/ManagePage.xaml.cs
+++ GameDev/GameDev/GameDev/Views/ManagePage.xaml.cs
@@ -7,34 +7,14 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
-using GameDev.Views.Characters;
-using GameDev.Views.Score;
-using GameDev.Views.Items;
-
 namespace GameDev.Views
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class ManagePage : ContentPage
-	{
-		public ManagePage ()
-		{
-			InitializeComponent ();
-            Title = "Manage";
-        }
-
-        async void OnItemsClicked(object sender, EventArgs args)
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ManagePage : TabbedPage
+    {
+        public ManagePage ()
         {
-            await Navigation.PushAsync(new ItemsPage());
-        }
-
-        async void OnCharactersClicked(object sender, EventArgs args)
-        {
-            await Navigation.PushAsync(new CharactersPage());
-        }
-
-        async void OnHistoryScoreClicked(object sender, EventArgs args)
-        {
-            await Navigation.PushAsync(new ScoresPage());
+            InitializeComponent();
         }
     }
 }
