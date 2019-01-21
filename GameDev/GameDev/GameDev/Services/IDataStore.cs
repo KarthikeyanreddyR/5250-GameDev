5bba1cae15048d7cb9664be3fce2d70d6de42051
Karthikeyan Reddy Rokkam <rokkamkarthi@seattleu.edu>
2019-01-20 17:56:47 -0800

    Modified MockDataStore

--- GameDev/GameDev/GameDev/Services/IDataStore.cs
+++ GameDev/GameDev/GameDev/Services/IDataStore.cs
@@ -2,14 +2,26 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
+using GameDev.Models;
+
 namespace GameDev.Services
 {
-    public interface IDataStore<T>
+    /*public interface IDataStore<T>
     {
         Task<bool> AddItemAsync(T item);
         Task<bool> UpdateItemAsync(T item);
         Task<bool> DeleteItemAsync(string id);
         Task<T> GetItemAsync(string id);
         Task<IEnumerable<T>> GetItemsAsync(bool forceRefresh = false);
+    }*/
+
+    public interface IDataStore
+    {
+        Task<bool> InsertUpdateAsync_Item(Item data);
+        Task<bool> AddAsync_Item(Item data);
+        Task<bool> UpdateAsync_Item(Item data);
+        Task<bool> DeleteAsync_Item(Item data);
+        Task<Item> GetAsync_Item(string id);
+        Task<IEnumerable<Item>> GetAllAsync_Item(bool forceRefresh = false);
     }
 }
--- GameDev/GameDev/GameDev/Services/MockDataStore.cs
+++ GameDev/GameDev/GameDev/Services/MockDataStore.cs
@@ -6,61 +6,102 @@ using GameDev.Models;
 
 namespace GameDev.Services
 {
-    public class MockDataStore : IDataStore<Item>
+    public class MockDataStore : IDataStore
     {
-        List<Item> items;
+        #region Singleton
+        // Make this a singleton so it only exist one time because holds all the data records in memory
+        private static MockDataStore _instance;
 
-        public MockDataStore()
+        public static MockDataStore Instance
         {
-            items = new List<Item>();
-            var mockItems = new List<Item>
+            get
             {
-                new Item { Id = Guid.NewGuid().ToString(), Text = "First item", Description="This is an item description." },
-                new Item { Id = Guid.NewGuid().ToString(), Text = "Second item", Description="This is an item description." },
-                new Item { Id = Guid.NewGuid().ToString(), Text = "Third item", Description="This is an item description." },
-                new Item { Id = Guid.NewGuid().ToString(), Text = "Fourth item", Description="This is an item description." },
-                new Item { Id = Guid.NewGuid().ToString(), Text = "Fifth item", Description="This is an item description." },
-                new Item { Id = Guid.NewGuid().ToString(), Text = "Sixth item", Description="This is an item description." },
-            };
-
-            foreach (var item in mockItems)
-            {
-                items.Add(item);
+                if (_instance == null)
+                {
+                    _instance = new MockDataStore();
+                }
+                return _instance;
             }
         }
 
-        public async Task<bool> AddItemAsync(Item item)
+        #endregion Singleton
+
+        private List<Item> itemsDataSet = new List<Item>();
+
+        public MockDataStore()
         {
-            items.Add(item);
+            CreateMockItemsDataSet();           
+        }
+
+        #region Items
+
+        private void CreateMockItemsDataSet()
+        {
+            // string name, string description, string imageUri, int range, int damage, AttributeEnum attribute, ItemLocationEnum location, int value
+            itemsDataSet.Add(new Item("Noisy Cricket", "Small, portable gun which make huge boom!!!", null, 0, 10, AttributeEnum.Attack, ItemLocationEnum.OffHand, 10));
+            itemsDataSet.Add(new Item("De-Atomizer", "Heavy machinery which can reduce opponents to atoms.", null, 0, 10, AttributeEnum.Attack, ItemLocationEnum.PrimaryHand, 10));
+            itemsDataSet.Add(new Item("Anti-Gravity Shoes", "Defy gravity and take your fight in air", null, 0, 10, AttributeEnum.Defense, ItemLocationEnum.Feet, 10));
+            itemsDataSet.Add(new Item("Shrink Ray", "Minimize damage you take from powerful attacks", null, 0, 10, AttributeEnum.Defense, ItemLocationEnum.RightFinger, 10));
+        }
+
+        public async Task<bool> InsertUpdateAsync_Item(Item data)
+        {
+            // Check to see if the item exist
+            var oldData = await GetAsync_Item(data.Id);
+            if (oldData == null)
+            {
+                itemsDataSet.Add(data);
+                return true;
+            }
+
+            // Compare it, if different update in the DB
+            var UpdateResult = await UpdateAsync_Item(data);
+            if (UpdateResult)
+            {
+                await AddAsync_Item(data);
+                return true;
+            }
+
+            return false;
+        }
+
+        public async Task<bool> AddAsync_Item(Item data)
+        {
+            itemsDataSet.Add(data);
+            return await Task.FromResult(true);
+        }
+
+        public async Task<bool> UpdateAsync_Item(Item data)
+        {
+            var myData = itemsDataSet.FirstOrDefault(arg => arg.Id == data.Id);
+            if (myData == null)
+            {
+                return false;
+            }
+
+            myData.Update(data);
 
             return await Task.FromResult(true);
         }
 
-        public async Task<bool> UpdateItemAsync(Item item)
+        public async Task<bool> DeleteAsync_Item(Item data)
         {
-            var oldItem = items.Where((Item arg) => arg.Id == item.Id).FirstOrDefault();
-            items.Remove(oldItem);
-            items.Add(item);
+            var myData = itemsDataSet.FirstOrDefault(arg => arg.Id == data.Id);
+            itemsDataSet.Remove(myData);
 
             return await Task.FromResult(true);
         }
 
-        public async Task<bool> DeleteItemAsync(string id)
+        public async Task<Item> GetAsync_Item(string id)
         {
-            var oldItem = items.Where((Item arg) => arg.Id == id).FirstOrDefault();
-            items.Remove(oldItem);
-
-            return await Task.FromResult(true);
+            return await Task.FromResult(itemsDataSet.FirstOrDefault(s => s.Id == id));
         }
 
-        public async Task<Item> GetItemAsync(string id)
+        public async Task<IEnumerable<Item>> GetAllAsync_Item(bool forceRefresh = false)
         {
-            return await Task.FromResult(items.FirstOrDefault(s => s.Id == id));
+            return await Task.FromResult(itemsDataSet);
         }
 
-        public async Task<IEnumerable<Item>> GetItemsAsync(bool forceRefresh = false)
-        {
-            return await Task.FromResult(items);
-        }
+        #endregion Items
     }
 }
