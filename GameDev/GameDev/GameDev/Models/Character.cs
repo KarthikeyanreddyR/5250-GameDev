5257cc6dc7df42ffe22630dfc7bcfc7749b55b27
Karthikeyan Reddy Rokkam <rokkamkarthi@seattleu.edu>
2019-01-30 15:11:16 -0800

    Added character models and viewModels

---
+++ GameDev/GameDev/GameDev/Models/Character.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameDev.Models
+{
+    public class Character : Entity<Character>
+    {
+        public int Age { get; set; }
+
+        public Character()
+        {
+            CreateDefaultCharacter();
+        }
+
+        public Character(string name, string description, string imageUri, int age)
+        {
+            CreateDefaultCharacter();
+
+            Name = name;
+            Description = description;
+            ImageURI = imageUri;
+            Age = age;
+        }
+
+        private void CreateDefaultCharacter()
+        {
+            Name = "Unknown";
+            Description = "Unknown";
+            ImageURI = null;
+            Age = 0;
+        }
+    }
+}
