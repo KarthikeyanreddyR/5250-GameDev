a6ade83b9af96467667f2f67a9d34d4c4b672a7e
Karthikeyan Reddy Rokkam <rokkamkarthi@seattleu.edu>
2019-01-30 17:21:02 -0800

    Characters Page initial commit

--- GameDev/GameDev/GameDev/ViewModels/CharacterViewModel.cs
+++ GameDev/GameDev/GameDev/ViewModels/CharacterViewModel.cs
@@ -1,10 +1,66 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Text;
+using System.Threading.Tasks;
+using GameDev.Models;
+using Xamarin.Forms;
 
 namespace GameDev.ViewModels
 {
-    class CharacterViewModel
+    public class CharacterViewModel : BaseViewModel
     {
+
+        #region Singleton
+        // Create one instance of view model
+        private static CharacterViewModel _instance;
+
+        public static CharacterViewModel Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = new CharacterViewModel();
+                return _instance;
+            }
+        }
+        #endregion Singleton
+
+        public ObservableCollection<Character> Dataset { set; get; }
+
+        public Command LoadItemsCommand { get; set; }
+
+        public CharacterViewModel()
+        {
+            Dataset = new ObservableCollection<Character>();
+            LoadItemsCommand = new Command(async () => await ExecuteLoadItemsCommand());
+        }
+
+        async Task ExecuteLoadItemsCommand()
+        {
+            if (IsBusy)
+                return;
+
+            IsBusy = true;
+
+            try
+            {
+                Dataset.Clear();
+                var characters = await DataStore.GetAllAsync_Character(true);
+                foreach (var charcter in characters)
+                {
+                    Dataset.Add(charcter);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
     }
 }
--- GameDev/GameDev/GameDev/Views/Characters/CharactersPage.xaml.cs
+++ GameDev/GameDev/GameDev/Views/Characters/CharactersPage.xaml.cs
@@ -7,15 +7,46 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
+using GameDev.ViewModels;
+using GameDev.Models;
+
 namespace GameDev.Views.Characters
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class CharactersPage : ContentPage
     {
+
+        CharacterViewModel viewModel;
+
         public CharactersPage()
         {
             InitializeComponent();
-            Title = "Characters";
+            BindingContext = viewModel = CharacterViewModel.Instance;
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            //if (viewModel.Items.Count == 0)
+            viewModel.LoadItemsCommand.Execute(null);
+        }
+
+        async void OnCharacterSelected(object sender, SelectedItemChangedEventArgs args)
+        {
+            if (!(args.SelectedItem is Character character))
+                return;
+
+           // await Navigation.PushAsync(new CharacterDetailPage(new CharacterDetailsViewModel(character)));
+
+            // Manually deselect item.
+            ItemsListView.SelectedItem = null;
+        }
+
+        private void Add_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new CharacterNewPage());
+
         }
     }
 }
