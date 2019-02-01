6d6183e9531170e5919d37a6998e896385d8d382
Karthikeyan Reddy Rokkam <rokkamkarthi@seattleu.edu>
2019-02-01 02:38:16 -0800

    added primary key

--- GameDev/GameDev/GameDev/Models/BaseEntity.cs
+++ GameDev/GameDev/GameDev/Models/BaseEntity.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using System.Text;
 
+using SQLite;
+
 namespace GameDev.Models
 {
     public class BaseEntity<T>
     {
+        [ PrimaryKey]
         public string Id { get; set; }
 
         public string Guid { get; set; }
