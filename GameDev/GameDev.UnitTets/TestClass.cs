16529e73abced35a2edc87285b5ba81d9353789e
Karthikeyan Reddy Rokkam <rokkamkarthi@seattleu.edu>
2019-02-06 23:25:11 -0800

    Added Unit Test project

---
+++ GameDev/GameDev.UnitTets/TestClass.cs
@@ -0,0 +1,25 @@
+﻿using GameDev.Models;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameDev.UnitTets
+{
+    [TestFixture]
+    public class TestClass
+    {
+        [Test]
+        public void TestMethod()
+        {
+            var character = new Character();
+
+            var expected = 20;
+            var actual = character.Age;
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
